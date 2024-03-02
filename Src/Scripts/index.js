29ba263961c0830735160de6bce240ddbc4069df
Valquiria Pereira dos Santos <valquiriapereiradossantos@gmail.com>
2024-03-02 14:18:08 -0300

    Iniciando o Js Jogo da velha

--- Src/Scripts/index.js
+++ Src/Scripts/index.js
@@ -1,1 +1,31 @@
-alert('blee')
+const game = {
+currentMove: 'X'
+
+}
+
+function getField(fieldNumber) {
+    const $field = document.querySelector('.scenary-field-' + fieldNumber)
+
+    return $field
+}
+
+function toggleCorrentMove() {
+
+    if (game.currentMove == 'X') {
+        game.currentMove = 'O'
+
+    } else if (game.currentMove == 'O') {
+        game.currentMove = 'X'
+    }
+
+}
+
+for (let i = 0; i < 9; i++) {
+    const $field = getField(i)
+
+    $field.addEventListener('click', function () {
+        $field.textContent = game.currentMove
+        toggleCorrentMove()
+    })
+}
+
