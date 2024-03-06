1c8e7cf66dc5a5e3bfb0a9294fcce207c3c282ed
Valquiria Pereira dos Santos <valquiriapereiradossantos@gmail.com>
2024-03-06 15:12:14 -0300

    continuando o js

--- Src/Scripts/index.js
+++ Src/Scripts/index.js
@@ -20,12 +20,32 @@ function toggleCorrentMove() {
 
 }
 
+function verifyFields(firstField, secondField, thirdField) {
+    const $fieldList = document.querySelectorAll('.scenary-field-big')
+   const hasWinner =$fieldList[firstField].textContent != ''
+   && $fieldList[firstField].textContent == $fieldList[secondField].textContent 
+    && $fieldList[secondField].textContent == $fieldList[thirdField].textContent
+
+return hasWinner
+}
+
+function getWinner() {
+    console.log(verifyFields(0, 1, 2))
+
+    //verifyFields()
+
+    //if (false){
+       // alert('blee')
+//  }
+}
+
 for (let i = 0; i < 9; i++) {
     const $field = getField(i)
 
-    $field.addEventListener('click', function () {
+
+    $field.addEventListener('click', function () {        
         $field.textContent = game.currentMove
         toggleCorrentMove()
     })
 }
-
+ getWinner()
